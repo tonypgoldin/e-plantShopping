77b8f2fc5b1f06d23c969d8cb7a19dd2e8e3ae1e
Tony Golding <tpg@blueyonder.co.uk>
2025-02-21 11:36:52 -0500

    Refactored unit tests

--- test/ProductList.test.js
+++ test/ProductList.test.js
@@ -4,44 +4,32 @@ import {act } from "react-dom/test-utils";
 import ProductList from "../src/ProductList";
 
 describe("Product List", () => {
+    let container;
+
+    const items = [{name: "Sunshine Plant", description: "Bright yellow like the sun", cost: "$18"}];
+
+    beforeEach(() => {
+        container = document.createElement("div");
+
+        document.body.replaceChildren(container);
+    });
+
+    const render = component => act(() => ReactDOM.createRoot(container).render(component));
+
     it("renders the name of the product", () => {
-        const items = [{name: "Sunshine Plant"}];
-
-        const component = (<ProductList products={items} />);
-
-        const container = document.createElement("div");
-
-        document.body.appendChild(container)
-
-        act(() => ReactDOM.createRoot(container).render(component));
+        render(<ProductList products={items} />);
   
         expect(document.body.textContent).toContain("Sunshine Plant");
     })
 
     it("renders the description of the product", () => {
-        const items = [{name: "Sunshine Plant", description: "Bright yellow like the sun"}];
+        render(<ProductList products={items} />);
 
-        const component = (<ProductList products={items} />);
-
-        const container = document.createElement("div");
-
-        document.body.appendChild(container)
-
-        act(() => ReactDOM.createRoot(container).render(component));
-  
         expect(document.body.textContent).toContain("Bright yellow like the sun");
     })
 
     it("renders the description of the product", () => {
-        const items = [{name: "Sunshine Plant", description: "Bright yellow like the sun", cost: "$18"}];
-
-        const component = (<ProductList products={items} />);
-
-        const container = document.createElement("div");
-
-        document.body.appendChild(container)
-
-        act(() => ReactDOM.createRoot(container).render(component));
+        render(<ProductList products={items} />);
   
         expect(document.body.textContent).toContain("$18");
     })
